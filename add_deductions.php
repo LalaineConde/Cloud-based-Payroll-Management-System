6261b4f8ee3e85fbf232c558d01a8c04483541b3
LalaineConde <161687040+LalaineConde@users.noreply.github.com>
2024-06-26 00:27:24 +0800

    update

--- add_deductions.php
+++ add_deductions.php
@@ -1,7 +1,7 @@
 <?php
 	
-		require("db.php");
-		
+		require 'connection.php';
+		$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 		@$id 			= $_POST['deduction_id'];
 		@$philhealth 	= $_POST['philhealth'];
 		@$bir 			= $_POST['bir'];
@@ -10,7 +10,7 @@
 		@$loans 		= $_POST['loans'];
 
 
-		$sql = mysql_query("UPDATE deductions SET bir='$bir', gsis='$gsis', pag_ibig='$love', loans='$loans', philhealth='$philhealth' WHERE deduction_id='1'");
+		$sql = mysqli_query($connection, "UPDATE deductions SET bir='$bir', gsis='$gsis', pag_ibig='$love', loans='$loans', philhealth='$philhealth' WHERE deduction_id='1'");
 
 		if($sql)
 		{
